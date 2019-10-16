8ba0b23eee3a43631496ca89e94b1dd5f6643480
Aslan <berezg86@gmail.com>
2019-10-17 01:04:16 +0300

    Форма связи, карточка товара

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -1,3 +1,29 @@
-$(document).ready(function(){
-    // alert(1);
+$(document).ready(function() {
+
+    $(".switch__button").on("click", function() {
+        var $button = $(this);
+        var element = $button.parent().find(".product__counter");
+        var oldValue = element.text();
+        if ($button.parent().hasClass('product__amount')) {
+            if ($button.text() == "+") {
+                var newVal = parseFloat(oldValue) + 1000;
+            } else {
+                if (parseFloat(oldValue) < 3000) {
+                    return;
+                }
+                var newVal = parseFloat(oldValue) - 1000;
+            }
+        }else{
+            if ($button.text() == "+") {
+                var newVal = parseFloat(oldValue) + 1;
+            } else {
+                if (parseFloat(oldValue) < 8) {
+                    return;
+                }
+                var newVal = parseFloat(oldValue) - 1;
+            }
+        }
+        $button.parent().find(".product__counter i").html(newVal);
+    });
+
 });
